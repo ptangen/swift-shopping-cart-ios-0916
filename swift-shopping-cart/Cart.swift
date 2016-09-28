c539cad3d13938bc79e3cc481b0e131f8a66360b
Paul Tangen <ptangen@ptangen.com>
2016-09-28 14:39:59 -0400

    Complete the shooping cart lab.

--- swift-shopping-cart/Cart.swift
+++ swift-shopping-cart/Cart.swift
@@ -7,6 +7,58 @@
 //
 
 class Cart {
+    var items: [Item] = []
     
+    func totalPriceInCents() -> Int {
+        var result = 0
+        for item in items{
+            result += item.priceInCents
+        }
+        return result
+    }
     
+    func add(item:Item) {
+        self.items.append(item)
+    }
+    
+    func remove(item:Item) {
+        for (index, itemInCart) in items.enumerated() {
+            if itemInCart.name == item.name && itemInCart.priceInCents == item.priceInCents {
+                self.items.remove(at: index)
+            }
+        }
+    }
+    
+    func items(withName name:String) -> [Item] {
+        var results: [Item] = []
+        
+        for item in self.items {
+            if name == item.name {
+                results.append(item)
+            }
+        }
+        return results
+    }
+    
+    func items(withMinPrice price:Int) -> [Item] {
+        var results: [Item] = []
+        
+        for item in self.items {
+            if price <= item.priceInCents {
+                results.append(item)
+            }
+        }
+        return results
+    }
+    
+    func items(withMaxPrice price:Int) -> [Item] {
+        var results: [Item] = []
+        
+        for item in self.items {
+            if price >= item.priceInCents {
+                results.append(item)
+            }
+        }
+        return results
+    }
 }
